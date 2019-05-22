5439c2c08225e5897d2daf243a842fd43be1c89a
Raisaat Rashid <raisaat96@gmail.com>
2019-05-22 16:30:21 +0600

    Update program3.c

--- program3.c
+++ program3.c
@@ -1,17 +1,17 @@
-/////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////////////
 // program3.c
 // Author: Raisaat Atifa Rashid
 //
 // Procedures:
 //      main				Creates 5 threads for 5 philosophers and initializes
-//							5 semaphores for 5 forks
-//		think				Lets a philosopher think
-//		eat					Lets a philosopher eat 
-//		philosopher			Allows each philosopher to think and eat. Allows
-//							philosophers to eat only when the forks on both the 
-//							philosopher's left and right are available. While the 
-//							philosopher is eating, those two forks are locked.
-/////////////////////////////////////////////////////////////////////////////////////////
+//					5 semaphores for 5 forks
+//	think				Lets a philosopher think
+//	eat				Lets a philosopher eat 
+//	philosopher			Allows each philosopher to think and eat. Allows
+//					philosophers to eat only when the forks on both the 
+//					philosopher's left and right are available. While the 
+//					philosopher is eating, those two forks are locked.
+///////////////////////////////////////////////////////////////////////////////////////////////////
 
 /* Required libraries */
 #include <stdio.h>
@@ -40,9 +40,9 @@ sem_t forks[NUMB_THREADS]; // Array of 5 semaphores for the 5 forks on the table
 // Author: Raisaat Atifa Rashid
 // Date: 10 March 2019
 // Description: Function that lets a philosopher think. Displays a message
-//				saying which philosopher is thinking. Sleeps for a random number
-//				of microseconds and after that, displays a message saying that
-//				the philosopher has finished thinking.
+//		saying which philosopher is thinking. Sleeps for a random number
+//		of microseconds and after that, displays a message saying that
+//		the philosopher has finished thinking.
 //
 // Parameters:
 //     n		I/P		int		The philosopher number
@@ -56,7 +56,7 @@ void think(int n)
 	
 	srand(time.tv_sec * time.tv_usec); // Seed the random number generator
 	usleep(rand() % 10000000);	// Let the philosopher think for a random number of microseconds between 0 and 10000000 
-								// microseconds by letting the function sleep for that number of seconds 
+					// microseconds by letting the function sleep for that number of seconds 
 	
 	printf("Philosopher %d has finished thinking.\n", n + 1); // Display a message saying that the philosopher has finished thinking
 
@@ -67,10 +67,10 @@ void think(int n)
 // Author: Raisaat Atifa Rashid
 // Date: 10 March 2019
 // Description: Function that lets a philosopher eat. Displays a message
-//				saying which philosopher is eating and the time at which the 
-//				philosopher starts eating. Sleeps for a random number of microseconds
-//				and after that, displays a message saying that the philosopher has 
-//				finished eating and the time at which the philosopher finishes eating.
+//		saying which philosopher is eating and the time at which the 
+//		philosopher starts eating. Sleeps for a random number of microseconds
+//		and after that, displays a message saying that the philosopher has 
+//		finished eating and the time at which the philosopher finishes eating.
 //
 // Parameters:
 //     n		I/P		int		The philosopher number
@@ -91,7 +91,7 @@ void eat (int n)
 	
 	srand(time_1.tv_sec * time_1.tv_usec); // Seed the random number generator
 	usleep(rand() % 10000000);	// Let the philosopher eat for a random number of microseconds between 0 and 10000000
-								// microseconds by letting the function sleep for that number of seconds
+					// microseconds by letting the function sleep for that number of seconds
 
 	time(&rawTime); // Get the raw current time
 	timeInfo = localtime(&rawTime); // Get the current local time
@@ -106,15 +106,15 @@ void eat (int n)
 // Author: Raisaat Atifa Rashid
 // Date: 10 March 2019
 // Description: Function that allows a philosopher to think and then eat. The function first
-//				allows the philosopher to think by calling function think(). It then
-//				uses a semaphore to lock the fork on the philosopher's left and another
-//				semaphore to lock the fork on the philosopher's right, and then allows the 
-//				philosopher to eat by calling function eat(). After the philosopher
-//				finishes eating, it unlocks the right fork's semaphore followed by the
-//				left fork's semaphore.
+//		allows the philosopher to think by calling function think(). It then
+//		uses a semaphore to lock the fork on the philosopher's left and another
+//		semaphore to lock the fork on the philosopher's right, and then allows the 
+//		philosopher to eat by calling function eat(). After the philosopher
+//		finishes eating, it unlocks the right fork's semaphore followed by the
+//		left fork's semaphore.
 //
 // Parameters:
-//		n				I/P		int			The philosopher number
+//		n			I/P		int		The philosopher number
 //		philosopher		O/P		void *		NULL pointer
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 void * philosopher(void * n)
@@ -141,10 +141,10 @@ void * philosopher(void * n)
 // Author: Raisaat Atifa Rashid
 // Date: 10 March 2019
 // Description: Initializes the semaphores for the 5 forks. Spawns 5 threads, 
-//				one for each of the 5 philosophers. It then sleeps for 300 
-//				seconds (5 minutes) to allow the threads to run for 300 seconds
-//				and then kills the threads after 300 seconds. After that it 
-//				joins the threads and finally, destroys the semaphores.
+//		one for each of the 5 philosophers. It then sleeps for 300 
+//		seconds (5 minutes) to allow the threads to run for 300 seconds
+//		and then kills the threads after 300 seconds. After that it 
+//		joins the threads and finally, destroys the semaphores.
 //
 // Parameters:
 //      main    O/P     int         status code
@@ -162,11 +162,11 @@ int main()
 	/* Loop NUMB_THREADS times to create 5 threads representing the 5 dining philosopher */
 	for (i = 0; i < NUMB_THREADS; i++)
 	{
-		s = pthread_create(&thread_id[i], NULL, philosopher, &philosopher_num[i]);	// Create a thread for each of the 5 philosophers by calling 
+		s = pthread_create(&thread_id[i], NULL, philosopher, &philosopher_num[i]); // Create a thread for each of the 5 philosophers by calling 
 																					// pthread_create(), and store the function's return value in s
 		if (s != 0) // If s is not equal to 0, i.e. if the pthread_create function failed to execute
 			handle_error_en(s, "pthread_create");	// Call error-handling macro with the error value in s 
-													// and an error message as arguments to terminate the program
+								// and an error message as arguments to terminate the program
 	}
 
 	sleep(300); // Sleep for 300 seconds (5 minutes) to allow the threads to run for 300 seconds
@@ -177,7 +177,7 @@ int main()
 		s = pthread_cancel(thread_id[i]);	// Cancel (and hence, kill) each thread by calling pthread_cancel(), and store the function's return value in s
 		if (s != 0) // If s is not equal to 0, i.e. if the pthread_cancel function failed to execute
 			handle_error_en(s, "pthread_cancel");	// Call error-handling macro with the error value in s 
-													// and an error message as arguments to terminate the program
+								// and an error message as arguments to terminate the program
 	}
 
 	/* Loop NUMB_THREADS times to wait for the 5 threads to join */
